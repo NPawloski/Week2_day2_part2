4f2f8e0d944fcacca589e0d7df14fca037506f74
Nathan Pawloski <natepawloski@gmail.com>
2017-10-04 10:32:24 -0400

    Initial Commit

---
+++ Week2_Day2_pt2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Day2_pt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Do you want to play the game? (YES/NO)");
+            //String playAgain = Console.ReadLine();
+            //playAgain = playAgain.ToUpper();
+
+            //while (playAgain == "YES")
+            //{
+            //    Console.WriteLine("IT's a rematch");
+            //    Console.WriteLine("Do you want to play the game? (YES/NO)");
+            //    playAgain = Console.ReadLine();
+            //    playAgain = playAgain.ToUpper();
+            //}
+
+            //Console.WriteLine("Would you like to check the patinet into their appointment? (YES/NO)");
+            //String checkPatient = Console.ReadLine().ToUpper();
+
+            //while (checkPatient != "YES")
+            //{
+            //    Console.WriteLine("PATIENT CHECK IN SYSTEM");
+            //    Console.WriteLine("What is the client's full name");
+            //    String patientName = Console.ReadLine();
+            //    Console.WriteLine("What is the clients 6-digit patient ID");
+            //    int patientId = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("What is the clients appointment time?");
+            //    String apointmentTime = Console.ReadLine();
+            //    Console.WriteLine("Would you like to check the patinet into their appointment? (YES/NO)");
+            //    checkPatient = Console.ReadLine().ToUpper();
+            //}
+
+
+
+
+
+
+        }
+    }
+}
